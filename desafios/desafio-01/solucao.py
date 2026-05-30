dee06aea28ba079eed5b7ab9adecf4f798e1fd4c
dev-gui47 <guicavalcante110@gmail.com>
2026-05-30 00:13:19 -0300

    alteracoes no desafio 1

--- desafios/desafio-01/solucao.py
+++ desafios/desafio-01/solucao.py
@@ -10,8 +10,18 @@ nome = input("Digite seu nome: ")
 ano_nascimento = int(input("Digite seu ano de nascimento: "))
 ano_atual = 2026
 idade = ano_atual - ano_nascimento
+hobbies = []
+while True:
+    hobbie = str(input('digite seus hobbies: '))
+    if hobbie == 'sair':
+        print('saindo...')
+        break
+    else: 
+        hobbies.append(hobbie)
+
 
 print(f"ola, {nome}! voce tem {idade} anos.")
+print(f"seus hobbies são: {', '.join(hobbies)}") #peguei do desafio 5, pelo que entendi ele junta os elementos da minha lista tudo em so uma string, e depois separando com virgula ", ";
 
 
 
